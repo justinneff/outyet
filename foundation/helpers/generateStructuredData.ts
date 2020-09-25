56b2778855d011c73cc702ab6894bd359137a908
John Murphy <jmichealmurphy@gmail.com>
2020-09-25 14:47:52 -0500

    create helper for structured data

---
+++ foundation/helpers/generateStructuredData.ts
@@ -0,0 +1,29 @@
+import format from 'date-fns/format'
+import { Book } from '../types/Book'
+
+export function generateStructuredData(
+	siteUrl: string,
+	bookImagePath: string,
+	book: Book,
+	releaseDate: Date
+) {
+	return `
+		{
+			"@context": "https://schema.org/",
+			"@type": "Event",
+			"name": "${book.title} Release",
+			"startDate": "${releaseDate ? format(releaseDate, 'yyyy-MM-dd') : ''}",
+			"endDate": "${releaseDate ? format(releaseDate, 'yyyy-MM-dd') : ''}",
+			"eventStatus": "https://schema.org/EventScheduled",
+			"eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode",
+			"location": {
+				"@type": "VirtualLocation",
+				"url": "${book.buy_links.book}"
+			},
+			"image": [
+				"${siteUrl}${bookImagePath}"
+			],
+			"description": "${book.description}"
+		}
+		`
+}
